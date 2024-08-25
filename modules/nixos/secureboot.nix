71e2470f8e2525bf5165360c2ee2a1cc3c3eb33b
John Rizzo <johnrizzo1@gmail.com>
2024-08-25 12:22:50 -0400

    added sbctl to start setting up secure boot

--- modules/nixos/secureboot.nix
+++ modules/nixos/secureboot.nix
@@ -1,16 +1,16 @@
 { pkgs, inputs, lib, ... }: {
-  imports = [
-    inputs.lanzaboote.nixosModules.lanzaboote
-  ];
+  # imports = [
+  #   inputs.lanzaboote.nixosModules.lanzaboote
+  # ];
 
   environment.systemPackages = [
     pkgs.sbctl # for troubleshooting SecureBoot
   ];
 
-  boot.loader.systemd-boot.enable = lib.mkForce false;
+  # boot.loader.systemd-boot.enable = lib.mkForce false;
 
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
-  };
+  # boot.lanzaboote = {
+  #   enable = true;
+  #   pkiBundle = "/etc/secureboot";
+  # };
 }
