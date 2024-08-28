1c9b06f8e6c20ee8604dfe25e2166eba45830299
John Rizzo <johnrizzo1@gmail.com>
2024-08-28 10:19:33 -0400

    updating config for nixalot

--- hosts/nixos/nixalot.nix
+++ hosts/nixos/nixalot.nix
@@ -1,10 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, modulesPath, ... }:
-
-{
+{ lib, config, pkgs, modulesPath, ... }: {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   networking.hostName = "nixalot"; # Define your hostname.
