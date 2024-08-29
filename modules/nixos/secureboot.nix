1fb90da8f3f5b29e9ebff80182f02fdcbeaa1f55
John Rizzo <johnrizzo1@gmail.com>
2024-08-29 16:28:23 -0400

    fixing some nixos related settings for secureboot to be optional

--- modules/nixos/secureboot.nix
+++ modules/nixos/secureboot.nix
@@ -1,23 +1,29 @@
 { config, pkgs, inputs, lib, ... }: {
-  environment.systemPackages = [
-    pkgs.sbctl # for troubleshooting SecureBoot
+  imports = [
+    inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  options = {
-    enable = mkEnbaleOption "secure boot";
+  options.services.secureboot = {
+    # enable = lib.mkEnableOption "secure boot";
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      example = true;
+      description = "enable secure boot?";
+    };
   };
 
   config = lib.mkIf config.services.secureboot.enable {
-    # imports = [
-    #   inputs.lanzaboote.nixosModules.lanzaboote
-    # ];
+    environment.systemPackages = [
+      pkgs.sbctl # for troubleshooting SecureBoot
+    ];
+
+    boot.loader.systemd-boot.enable = lib.mkForce false;
+    boot.loader.efi.canTouchEfiVariables = true;
 
     boot.lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
     };
-  }
+  };
 }
