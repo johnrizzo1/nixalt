f2827af2c3cf7705b36bb89a05ea31dd401e8fdb
John Rizzo <johnrizzo1@gmail.com>
2024-10-24 20:14:58 -0400

    adding amdgpu

--- machines/hardware/irl.nix
+++ machines/hardware/irl.nix
@@ -8,7 +8,7 @@
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
+  boot.initrd.kernelModules = ["amdgpu"];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
