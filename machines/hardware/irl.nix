68c5d473ed4df59a2a4b75ee1a5df49ce026168f
John Rizzo <johnrizzo1@gmail.com>
2024-10-24 20:57:31 -0400

    working on nvidia driver

--- machines/hardware/irl.nix
+++ machines/hardware/irl.nix
@@ -8,7 +8,7 @@
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["nvidia"];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
