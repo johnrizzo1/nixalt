207f483d79e582ba2166840fc1306958039bf2a1
John Rizzo <johnrizzo1@gmail.com>
2025-01-29 08:43:04 -0500

    - disabled nix-direnv - added microsoft teams - added kdenlive

--- users/jrizzo/home-manager.nix
+++ users/jrizzo/home-manager.nix
@@ -172,7 +172,7 @@ in
       enableBashIntegration = true;
       enableZshIntegration = true;
       # enableFishIntegration = true;
-      nix-direnv.enable = true;
+      # nix-direnv.enable = true;
       silent = true;
       config = {
         load_dotenv = true;
