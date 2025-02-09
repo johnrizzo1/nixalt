1580fc750d9caa8e36c91ddb720dc48d2a9e5d66
John Rizzo <johnrizzo1@gmail.com>
2025-02-09 00:37:16 -0500

    added postgresql and element-desktop packages.

--- machines/tymnet.nix
+++ machines/tymnet.nix
@@ -99,6 +99,8 @@
       virt-manager
       dotnet-sdk
       dbeaver-bin
+      postgresql
+      element-desktop
       # needed to add this to darwin-rebuild.. --option extra-sandbox-paths /nix/store
       # https://github.com/NixOS/nix/issues/4119
       # texlive.combined.scheme-full
