e619f1068ce9acf998d79e42d95f47d32e12df57
John Rizzo <johnrizzo1@gmail.com>
2025-10-27 19:17:20 -0400

    blah

--- machines/irl.nix
+++ machines/irl.nix
@@ -187,13 +187,13 @@
     #   '';
     # };
 
-    #k3s = {
-    #  enable = true;
-    #  role = "server";
+    k3s = {
+      enable = true;
+      role = "server";
     #  extraFlags = toString [
     #    # "--debug" # Optionally add additional args to k3s
     #  ];
-    #};
+    };
 
     #unbound = {
     #  enable = true;
